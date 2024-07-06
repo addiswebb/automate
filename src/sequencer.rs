f0b86dc4c04967cb1312d5bdead8cf31169d2920
Addis <akwgma@gmail.com>
2024-07-06 12:24:06 +0100

    Update sequencer.rs

--- src/sequencer.rs
+++ src/sequencer.rs
@@ -1018,7 +1018,7 @@ impl Sequencer {
                 })
                 .body(|mut body| {
                     body.row(ROW_HEIGHT, |mut row| {
-                        // row.set_hovered(true);
+                        row.set_hovered(true);
                         row.col(|_| {});
                         row.col(|ui| {
                             let rect = ui.max_rect();
