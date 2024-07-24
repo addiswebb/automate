a07f76fb11bdbf363b85996f8b41363ad448aa08
Addis <akwgma@gmail.com>
2024-07-24 22:14:52 +0100

    Change to using full path

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "untitled - Automate",
+        "Automate",
         native_options,
         Box::new(|cc| Ok(Box::new(automate::App::new(cc)))),
     )
